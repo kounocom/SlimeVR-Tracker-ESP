7fcf848e36f89e66ebcdca96608b0b8c45c79a59
Gorbit99 <gorbitgames@gmail.com>
2024-03-23 23:30:31 +0100

    Added ON-OFF button support

--- src/sensors/bmi160sensor.h
+++ src/sensors/bmi160sensor.h
@@ -176,6 +176,7 @@ class BMI160Sensor : public Sensor {
 
         bool getTemperature(float* out);
 
+        void deinitialize() override final;
     private:
         BMI160 imu {};
         int axisRemap;
